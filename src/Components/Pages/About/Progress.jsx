6d7e63ef01cdc826cbbada237f7e96003add9ad3
aminul157246 <aminul.islam.157246@gmail.com>
2024-04-04 11:51:47 +0600

    responsive done in about page

--- src/Components/Pages/About/Progress.jsx
+++ src/Components/Pages/About/Progress.jsx
@@ -1,35 +1,34 @@
-import React from 'react';
 
 const Progress = () => {
     return (
-        <div className='flex bg-blue-50 mx-12 p-12 justify-center my-16 gap-16'>
+        <div className='lg:flex bg-blue-50 mx-12 p-12 justify-center my-16 gap-16'>
             <div className='space-y-3'>
                 <p className='text-[#F4A492] text-xl    '>Bridely Fun Facts</p>
-                <h2 className='text-6xl font-bold'>Exploring our <br />  Wedding Service</h2>
+                <h2 className='text-3xl lg:text-6xl font-bold'>Exploring our <br />  Wedding Service</h2>
                 <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipiscin gelit <br /> sed doeiusm tempor incididunt</p>
 
             </div>
 
 
-            <div className='space-y-3'>
+            <div className='space-y-3 mt-12 lg:mt-0'>
                 <div>
                     <p className="text-xl">Guarantee</p>
-                    <progress className="progress progress-success w-96" value="70" max="100"></progress>
+                    <progress className="progress progress-success lg:w-96" value="70" max="100"></progress>
                 </div>
 
                 <div>
                     <p className="text-xl">Experience And Quality</p>
-                    <progress className="progress progress-success  w-96" value="80" max="100"></progress>
+                    <progress className="progress progress-success  lg:w-96" value="80" max="100"></progress>
                 </div>
 
                 <div>
                     <p className="text-xl">Food Ordering</p>
-                    <progress className="progress progress-success w-96" value="90" max="100"></progress>
+                    <progress className="progress progress-success lg:w-96" value="90" max="100"></progress>
                 </div>
 
                 <div>
                 <p className="text-xl">Professionality</p>
-                <progress className="progress progress-success w-96" value="100" max="95"></progress>
+                <progress className="progress progress-success lg:w-96" value="100" max="95"></progress>
                 </div>
             </div>
 
--- src/Components/Pages/About/WhatWeDo.jsx
+++ src/Components/Pages/About/WhatWeDo.jsx
@@ -6,24 +6,24 @@ import img3 from '../../../assets/images/wine.jpg'
 
 const WhatWeDo = () => {
     return (
-        <div className='pt-12 pb-40 gap-48 bg-blue-50 flex mx-12 justify-center my-16 items-center'>
+        <div className='pt-12 w-full pb-40 gap-48 bg-blue-50 lg:flex mx-12 justify-center my-16 items-center'>
 
-<div className='relative'>
-<div className='' ><img className='w-full h-full rounded-bl-[70px] border-8 border-white rounded-tr-[70px] ' src={img1} alt="" /></div>
-<div className=' absolute w-96 ml-72 top-56'><img className=' rounded-bl-[70px] border-8 border-white rounded-tr-[70px]' src={img2} alt="" /></div>
-<div className=' absolute top-96'><img className='w-full h-full rounded-bl-[70px] border-8 border-white rounded-tr-[70px]' src={img3} alt="" /></div>
-</div>
+            <div className='relative px-12'>
+                <div className='' ><img className='w-72 lg:w-full h-full rounded-bl-[70px] border-8 border-white rounded-tr-[70px] ' src={img1} alt="" /></div>
+                <div className=' absolute w-44 lg:w-96 ml-36 lg:ml-72 top-40 lg:top-56'><img className=' rounded-bl-[70px] border-8 border-white rounded-tr-[70px]' src={img2} alt="" /></div>
+                <div className=' lg:absolute top-96'><img className='w-44 lg:w-full h-full rounded-bl-[70px] border-8 border-white rounded-tr-[70px]' src={img3} alt="" /></div>
+            </div>
 
-                <div className='space-y-4'>
-                <img className='ml-80' src={bird} alt="" />
+            <div className='space-y-4'>
+                <img className='lg:ml-80 ml-12 mt-12 lg:mt-0' src={bird} alt="" />
 
-                <p className='text-[#F4A492] text-xl'>About Bridely</p>
-                <h2 className='text-6xl font-bold'>What We do, <br /> We do With Passion</h2>
-                <p className="text-xl">
+                <p className='text-[#F4A492] px-12 text-xl'>About Bridely</p>
+                <h2 className='text-3xl  px-12  lg:text-6xl font-bold'>What We do, <br /> We do With Passion</h2>
+                <p className="text-xl px-12 ">
                     Lorem ipsum dolor sit amet consectetur adipiscing elit <br /> sed doeiusm tempor incididunt donec quam felis ultri <br /> cies nec, pellentesque eu, pretium quis sem.
                 </p>
 
-                </div>
+            </div>
         </div>
     );
 };
